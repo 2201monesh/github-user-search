4afeced416e8862871b2760a22e36037c43320cf
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-05 22:30:24 +0530

    Search Section

--- src/UserSearch.js
+++ src/UserSearch.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { RiSearchLine } from 'react-icons/ri';
 
 function UserSearch() {
   return (
     <div className='user-search'>
-      user search
+     <div className="user-search-left">
+      <RiSearchLine className='search-icon' />
+      <input type="text" className='search-text' placeholder='Search Github username...' />
+     </div>  
+     <div className="user-search-right">
+        <button>Search</button>
+     </div>
     </div>
   )
 }
