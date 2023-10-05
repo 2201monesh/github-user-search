e977ccaabaa83413fb7aead07edd39fa7c8bc186
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-06 01:27:06 +0530

    User card UI

--- src/UserCard.js
+++ src/UserCard.js
@@ -3,7 +3,32 @@ import React from 'react'
 function UserCard() {
   return (
     <div className='user-card'>
-      userCard
+      <div className="avatar-img">Avatar image</div>
+      <div className="info-section">
+        <div className="info-section-heading">
+          <p className='fullname'>Full Name</p>
+          <p>Joined date</p>
+        </div>
+        <p className='username'>Username</p>
+        <p className='bio'>Bio</p>
+
+        <div className="follower-box">
+          <div className="repos">
+            <p>Repos</p>
+            <p>0</p>
+          </div>
+          <div className="followers">
+            <p>followers</p>
+            <p>100</p>
+          </div>
+          <div className="following">
+            <p>following</p>
+            <p>100</p>
+          </div>
+        </div>
+
+        <div className="socials"></div>
+      </div>
     </div>
   )
 }
